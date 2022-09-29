9beced57ed5a45f239eea6acf43465f46e290110
aadarsh805 <aadarshthakur80501@gmail.com>
2022-09-29 16:33:07 +0530

    styles and image  config, declaraation.d.ts file to handle file types

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import "./styles.css";
+import IMAGE from "./Konachan.jpg";
+import GOKU from './goku.svg'
+
 export const App = () => {
-    return (
-        <h1>React typescript webpack</h1>
-    )
-}
+  return <>
+    <h1>React typescript webpack</h1>
+    <img src={IMAGE} alt="konachan" width='300' height='200' />
+    <img src={GOKU} alt="goku" width='300' height='200' />
+  </>;
+};
