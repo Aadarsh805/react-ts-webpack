5812bf49e6c4d7170fd1896c3b2cd9db04a7d880
aadarsh805 <aadarshthakur80501@gmail.com>
2022-10-01 19:37:23 +0530

    eslint setup

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ import GOKU from "./goku.svg";
 import { ClickCounter } from "./ClickCounter";
 
 export const App = () => {
+  const num = "aadarsh";
+
   return (
     <>
       <h1>
-        React typescript webpack - {process.env.NODE_ENV} {process.env.name}
+        Reacta typescript webpack - {process.env.NODE_ENV} {process.env.name}
       </h1>
       <img src={IMAGE} alt="konachan" width="300" height="200" />
       <img src={GOKU} alt="goku" width="300" height="200" />
