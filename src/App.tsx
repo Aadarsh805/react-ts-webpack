19b71a2715abace335e73c215bcd2db7e01ea84d
aadarsh805 <aadarshthakur80501@gmail.com>
2022-10-01 19:16:00 +0530

    prod, dev webpack merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import "./styles.css";
 import IMAGE from "./Konachan.jpg";
-import GOKU from './goku.svg'
+import GOKU from "./goku.svg";
 
 export const App = () => {
-  return <>
-    <h1>React typescript webpack</h1>
-    <img src={IMAGE} alt="konachan" width='300' height='200' />
-    <img src={GOKU} alt="goku" width='300' height='200' />
-  </>;
+  return (
+    <>
+      <h1>
+        React typescript webpack - {process.env.NODE_ENV} {process.env.name}
+      </h1>
+      <img src={IMAGE} alt="konachan" width="300" height="200" />
+      <img src={GOKU} alt="goku" width="300" height="200" />
+    </>
+  );
 };
